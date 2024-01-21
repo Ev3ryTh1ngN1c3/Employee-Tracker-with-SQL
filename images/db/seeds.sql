057e043277b4af1d7d128330d649428cbc6a6ee1
Lisa <143395934+Ev3ryTh1ngN1c3@users.noreply.github.com>
2024-01-20 22:56:38 -0500

    adjusted SQL statements in the seeds.sql file

--- images/db/seeds.sql
+++ images/db/seeds.sql
@@ -1,31 +1,26 @@
--- insert departments
-INSERT INTO departments (department_name)
-VALUES 
-('Curatorial'),
-('Education Specialist'),
-('Collection Management'),
-('Marketing'),
-('Development'),
-('Security'),
-('Facilities');
+-- departments table
+INSERT INTO department (id, name) VALUES
+(1, 'Gallerists 1'),
+(2, 'Gallery Directors 2'),
+(3, 'Gallery Technicians 3');
 
 -- insert roles
 INSERT INTO role (id, title, salary, department_id) VALUES
 (1, 'Curatorial', 94000, 1),
-(2, 'Education Specialist', 60000, 2),
-(3, 'Collection Management', 160000, 3),
-(4, 'Marketing', 97000, 4),
-(5, 'Development', 100000, 5),
-(6, 'Security', 38000, 6),
-(7, 'Facilities', 70000, 7);
+(2, 'Education Specialist', 60000, 1),
+(3, 'Collection Management', 160000, 1),
+(4, 'Marketing', 97000, 2),
+(5, 'Development', 100000, 2),
+(6, 'Security', 38000, 3),
+(7, 'Facilities', 70000, 3);
 
 -- insert employees
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-('Leonardo', 'da Vinci', 1, 1),
-('Vincent', 'Van Gogh', 2, 2),
-('Claude', 'Monet', 3, 3),
-('Jackson', 'Pollock', 4, 4),
-('Salvador', 'Dalí', 5, 5),
-('Henri', 'Matisse', 6, 6),
-('Andy', 'Warhol', 7, 7);
+(1, 'Leonardo', 'da Vinci', 1, 1),
+(2, 'Vincent', 'Van Gogh', 2, 1),
+(3, 'Claude', 'Monet', 3, 1),
+(4, 'Jackson', 'Pollock', 4, 2),
+(5, 'Salvador', 'Dalí', 5, 2),
+(6, 'Henri', 'Matisse', 6, 3),
+(7, 'Andy', 'Warhol', 7, 3);
