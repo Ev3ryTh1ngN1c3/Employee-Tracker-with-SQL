7eaf24278ac006e69bf918ba9d9d04a341c20590
Lisa <143395934+Ev3ryTh1ngN1c3@users.noreply.github.com>
2024-01-20 21:23:30 -0500

    added comments to SQL script in the schema.sql file

--- images/db/schema.sql
+++ images/db/schema.sql
@@ -1,22 +1,25 @@
-CREATE TABLE department (
-  id INT PRIMARY KEY,
-  name VARCHAR(30)
+-- this script creates the database schema for the Employee Tracker project
+
+-- the departments table
+CREATE TABLE departments (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  department_name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE role (
-  id INT PRIMARY KEY,
-  title VARCHAR(30),
-  salary DECIMAL,
+-- the roles table
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255),
+  salary DECIMAL(10,2),
   department_id INT,
-  FOREIGN KEY (department_id) REFERENCES department(id)
+  FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
 );
 
+-- the employee table
 CREATE TABLE employee (
-  id INT PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
   role_id INT,
-  manager_id INT,
-  FOREIGN KEY (role_id) REFERENCES role(id),
-  FOREIGN KEY (manager_id) REFERENCES employee(id)
+  manager_id INT NOT NULL
 );
